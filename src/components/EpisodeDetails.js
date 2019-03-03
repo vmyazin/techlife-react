136383161160d45cff269f9b967d7c15f9da4660
Vasily Myazin <vasily@automotivemastermind.com>
2019-03-04 03:44:24 +0700

    Add SoundManager; player on episode page

--- src/components/EpisodeDetails.js
+++ src/components/EpisodeDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SoundPlayer from "./SoundPlayer";
 
 export class EpisodeDetails extends React.Component {
 
@@ -12,9 +13,7 @@ export class EpisodeDetails extends React.Component {
         <h3><span className="small-caps date">{item.pubDateConverted}</span></h3>
         <h2>№{item.episodeNum} {item.title}</h2>
         <p className="m-b-2"><a href={item.enclosure[0].$.url}><span className="mask-link-style"><img src="/images/icon-download.svg" alt="Download" /> </span>Скачать</a></p>
-        <div className="player">
-          <p id="play" className="btn-play">Play</p>
-        </div>
+        <SoundPlayer audioUrl={item.enclosure[0].$.url} />
 
         <section className="episode-desc">
           <div dangerouslySetInnerHTML={{__html: item.description}}></div>
---
+++ src/components/SoundPlayer.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Sound from 'react-sound';
+ 
+class SoundPlayer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      audioUrl: null,
+      playStatus: Sound.status.STOPPED,
+      elapsed: 0,
+      total: 0,
+      playFromPosition: 0,
+      lastPause: 0,
+      btnClass: ''
+    };
+  }
+
+  handleSongPlaying() {
+    //
+  }
+
+  clickToTogglePlay() {
+    if(this.state.playStatus === Sound.status.PLAYING){
+      // Pause if playing
+      this.setState({
+        playStatus: Sound.status.PAUSED,
+        btnClass: ""
+      })
+    } else {
+      // Resume if paused
+      this.setState({
+        playStatus: Sound.status.PLAYING,
+        btnClass: "is-playing"
+      })
+    }
+    console.log(this.state.playStatus);
+   }
+
+   togglePlayClass() {
+     console.log('lll');
+   }
+
+  render() {
+    let file = this.props.audioUrl;
+
+    return (
+      <div className="player">
+        <p className={"btn-play " + this.state.btnClass} onClick={this.clickToTogglePlay.bind(this)}>Play</p>
+
+        <Sound
+          url={file}
+          playStatus={this.state.playStatus}
+          onLoading={this.handleSongLoading}
+          onPlaying={this.handleSongPlaying}
+          onFinishedPlaying={this.handleSongFinishedPlaying}
+        />
+      </div>
+    )
+  }
+}
+
+export default SoundPlayer;
